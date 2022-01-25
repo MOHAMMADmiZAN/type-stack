e50dead70564a5441218c154afffa93af6dd312f
mohammadmiZAN <takbir.jcd@gmail.com>
2022-01-25 23:48:57 +0600

    lib update

--- src/index.ts
+++ src/index.ts
@@ -50,28 +50,40 @@ let btn = document.getElementById('btn')
 
  // recursive way
  function increment() {
+     function getRandomColor() {
+         const hexLetters = 'abcdef1234567890';
+         let hex = '#';
+         for (let i = 1; i <= 6; i++) {
+             hex += hexLetters[Math.floor(Math.random() * hexLetters.length)];
+         }
+         return hex;
+     }
      let box = document.getElementById('box_c')
      let width= box.style.width.split('%')
      if (parseInt(width[0])<100){
         setTimeout(() =>{
             box.style.width = String(parseInt(width[0])+5)+'%'
+            box.innerHTML= String(parseInt(width[0])+5)+'%'
+            box.style.background = getRandomColor()
             return increment()
-        },1000)
+        },300)
      }else{
          let reset = document.createElement('button')
          reset.style.cssText = `
          padding: 10px 15px;
          margin-left: 10px;
-         background-color: gray;
          outline: none;
          border-radius: 5px;
          border:none;
          cursor: pointer;
+         border: 1px solid darkred;
+         color: red;
          `
          reset.innerText = "Reset"
          btn.after(reset)
          reset.addEventListener('click',()=>{
              box.style.width = '5%'
+             box.innerHTML = '5%'
              btn.removeAttribute('disabled',)
              reset.remove()
 
