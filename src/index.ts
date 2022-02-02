cf10b007d371d0da7f56c1ba4635538fbbedfb65
mohammadmiZAN <takbir.jcd@gmail.com>
2022-02-02 16:02:17 +0600

    some arrays trick

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,10 @@ import {Player} from "./class";
 
 import {arrayAccumulation, cName, CounterUpOptions, objectAccumulation} from "./interface";
 
-const PlayerOne = new Player('Sachin', 40,cName.IN)
+const PlayerOne = new Player('Sachin', 40, cName.IN)
 
 const PlayerTwo = new Player('Tamim', 28)
-const PlayerThree = new Player('Afradi',40,cName.PK)
+const PlayerThree = new Player('Afradi', 40, cName.PK)
 
 PlayerOne.call()
 PlayerTwo.call()
@@ -48,7 +48,6 @@ counterUp({c: 'h1', t: 1000})
 counterUp({c: ps, t: 500})
 
 
-
 let btn = document.getElementById('btn')
 
 function getRandomColor() {
@@ -59,6 +58,7 @@ function getRandomColor() {
     }
     return hex;
 }
+
 // recursive way
 function increment() {
 
@@ -71,7 +71,7 @@ function increment() {
             box.innerHTML = String(parseInt(width[0]) + 5) + '%'
             box.style.background = getRandomColor()
             return increment()
-        }, 300)
+        }, 100)
     } else {
         let reset = document.createElement('button')
         reset.style.cssText = `
@@ -119,8 +119,7 @@ let obj = {
 
 function objectValue(v: object, t?: number) {
     let val = Object.entries(v)
-    let len = val.length
-    if (len > 0) {
+    if (val.length > 0) {
         setTimeout(() => {
             console.log(val[0][1])
             val = val.slice(1)
@@ -136,9 +135,7 @@ function objectValue(v: object, t?: number) {
 // object index iterate //
 function objectIndex(v: object, t?: number) {
     let val = Object.entries(v)
-    let len = val.length
-
-    if (len > 0) {
+    if (val.length > 0) {
         setTimeout(() => {
             console.log(val[0][0])
             val = val.slice(1)
@@ -148,42 +145,45 @@ function objectIndex(v: object, t?: number) {
     }
 
 }
+
 // objectIndex(obj,1000)
 
-const text:string = `Hello World`
- let textArray : string[] = text.split('')
+const text: string = `Hello World`
+let textArray: string[] = Array.from(text)
 
+let t = textArray.filter(v => v != text.charAt(5))
 
-
-console.log( typeof textArray)
-for (let [v,i] of text){
+console.log(`result:`, t)
+for (let [v, i] of text) {
     const p = document.createElement('p')
-    p.setAttribute('id', v+Math.floor(Math.random()*1000).toString())
-   p.innerHTML = v
+    p.setAttribute('id', v + Math.floor(Math.random() * 1000).toString())
+    p.innerHTML = v
     p.style.cssText = `background-color:${getRandomColor()};
     color:${getRandomColor()}
     `
     btn.before(p)
 
 }
- const review :number[]= [5,5,5,5,5,5,5,4,3,2,5,6,5]
+const review: number[] = [5, 5, 5, 5, 5, 5, 5, 4, 3, 2, 5, 5, 5,5, 5, 5, 5, 5, 5, 5, 4, 3, 2, 5, 5, 5,5, 5, 5, 5, 5, 5, 5, 4, 3, 2, 5, 5, 5]
+
+const map = new Map<string, string>(Object.entries(obj))
 
 
+console.log(map)
 
- let sum = review.reduce(function (acc,curr,index) {
-     acc += curr
-     return acc
- })
-console.log(sum)
-let avg = review.reduce(function (acc,curr,) {
-     acc+=curr/review.length
+let sum = review.reduce(function (acc, curr) {
+    acc += curr
     return acc
-},0)
+})
+console.log(sum)
+let avg = review.reduce(function (acc, curr,) {
+    acc += curr / review.length
+    return acc
+}, 0)
 console.log(avg)
 
 
-
-let names:string[] = [
+let names: string[] = [
     "java",
     "javascript",
     "java",
@@ -192,24 +192,117 @@ let names:string[] = [
     "php"
 ]
 const acc: objectAccumulation = {};
-const setArray:arrayAccumulation = []
+const setArray: arrayAccumulation = []
 
-let rate = names.reduce(function (acc,curr) {
-    acc[curr]?acc[curr]++:acc[curr] = 1
+let rate = names.reduce(function (acc, curr) {
+    acc[curr] ? acc[curr]++ : acc[curr] = 1
     return acc
- },acc)
+}, acc)
 console.log(rate)
 
-let unique  = names.reduce(function (acc,curr){
-    if (!acc.includes(curr)){
+let unique = names.reduce(function (acc, curr) {
+    if (!acc.includes(curr)) {
         acc.push(curr)
     }
     return acc
+}, setArray)
+
+console.log(unique)
+
+
+// My Reduce Function //
+const myReducer = (arr: any[], callback: (acc:any,curr:any,i:number,arr:any[])=>any, ac?: any) => {
+    let acc = ac
+    let start = 0
+    if (!acc) {
+        acc = arr[0]
+        start = 1
+    }
+    for (let i = start; i < arr.length; i++) {
+        acc = callback(acc, arr[i], i, arr)
+    }
+
+    return acc
+
+
+}
+let p = myReducer(review, function (acc, curr) {
+
+    return acc += curr
+
+})
+console.log(p)
+
+/*
+ * @forEach
+ */
+function myForeach(arr: any[],callback:(v:string,i:number,arr:any[])=>(string|number|void)) {
+
+    for (let i = 0; i < arr.length; i++) {
+        callback(arr[i], i,arr)
+    }
+
+}
+
+
+
+let u = myReducer(names,function (acc:any, curr:any) {
+    if (!acc.includes(curr)) {
+        acc.push(curr)
+    }
+
+    return acc
+
 },setArray)
 
-console.log(unique) /// set unique
-// make object
+console.log(u)
 
+
+ //  map
+ function myMap(arr: any[], cb:(v:string,i:number,arr:any[])=>(any|void)) {
+    let a = []
+     for (let i = 0; i <arr.length ; i++) {
+        a.push( cb(arr[i],i,arr))
+     }
+     return a
+ }
+
+let o = myMap(names,function (v,i) {
+    return new Map<string, (string | number)>([["id", i], ["value", v]])
+ })
+
+
+console.dir(o)
+
+let opj = new Map()
+
+console.dir(opj)
+// find average rating in 5 star
+function findAverageRating(arr: number[]) {
+    let sum = arr.reduce(function (acc, curr) {
+        acc += curr
+        return acc
+    }, 0)
+    return sum / arr.length
+}
+
+
+// my array red function
+function myReduce(arr: number[], cb: (acc: number, curr: number, i: number, arr: number[]) => number, ac?: number) {
+    let acc = ac
+    let start = 0
+    if (!acc) {
+        acc = arr[0]
+        start = 1
+    }
+    for (let i = start; i < arr.length; i++) {
+        acc = cb(acc, arr[i], i, arr)
+    }
+    return acc
+}
+
+let r = findAverageRating(review)
+console.log(r.toPrecision(2))
 
 
 
